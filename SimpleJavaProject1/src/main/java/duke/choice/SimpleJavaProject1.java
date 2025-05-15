b7854677c0fc5fddadd72dfd34cd630d67b00087
Jayani Adikari <mjmadikari@students.nsbm.ac.lk>
2025-05-15 22:20:36 +0530

    New java practice project

---
+++ SimpleJavaProject1/src/main/java/duke/choice/SimpleJavaProject1.java
@@ -0,0 +1,12 @@
+ 
+
+package duke.choice;
+public class SimpleJavaProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Customer c1= new Customer();
+        c1.name="pinkey";
+        System.out.println("Customer is pinkey");
+    }
+}
